b184f9ba6766b960b71f216f9bbf14c3a5b92fce
nitin197 <119704919+nitin197@users.noreply.github.com>
2022-12-02 20:47:59 -0500

    Add files via upload

---
+++ shape.java
@@ -0,0 +1,16 @@
+package Repo;
+
+public abstract class shape {
+
+	
+	public   abstract double CalArea(double a,double b);
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		
+		
+		
+	}
+
+}
