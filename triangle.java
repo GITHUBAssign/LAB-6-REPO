58daf1ed96a051057b14a7a248a430ece03b38e1
Ayush3366 <62565861+Ayush3366@users.noreply.github.com>
2022-12-05 09:20:27 -0500

    Add files via upload

---
+++ triangle.java
@@ -0,0 +1,25 @@
+package Repo;
+
+import java.util.Scanner;
+
+public class triangle  extends shape{
+	@Override
+	public double CalArea(double base, double height) {
+		double area=(base*height)/2;
+		return area;
+	}
+	
+	public static void main(String[] args) {
+		Scanner sc= new Scanner(System.in);
+		triangle tr= new triangle();
+		
+		System.out.println("Enter base and height of triangle");
+		double base=sc.nextDouble();
+		double height=sc.nextDouble();
+		
+		System.out.println("Area of rhombus :"+tr.CalArea(base, height));
+	}
+		
+	}
+
+	
